3a11ded03c400d483a19598ca204f82b9fd2ac98
mac-222 <mac-222@mac-222s-Mac-mini.local>
2018-04-18 16:48:21 +0300

    CR-3 added a common response format

---
+++ util/response.go
@@ -0,0 +1,34 @@
+package util
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Response struct represents base response format
+type Response struct {
+	Success bool        `json:"success"`
+	Data    interface{} `json:"data"`
+	Error   ErrorField  `json:"error"`
+	Meta    interface{} `json:"meta"`
+}
+
+// ErrorField provides nullable error type
+type ErrorField struct {
+	Err error
+}
+
+// MarshalJSON implements Marshaler interface to return nil in case there was no error
+func (ef ErrorField) MarshalJSON() ([]byte, error) {
+	if ef.Err != nil {
+		return json.Marshal(ef.Err.Error())
+	}
+	return json.Marshal(nil)
+}
+
+// JSON send json response to a client
+func JSON(r Response, w http.ResponseWriter, status int) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(r)
+}
